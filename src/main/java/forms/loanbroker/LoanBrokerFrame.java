135a62a3fe190be50bd3df92164ed673172a3086
Maiko Kingma <maiko.kingma@outlook.com>
2017-04-01 15:01:20 +0200

    Create listenToRequests and sendBankInterestRequest

--- src/main/java/forms/loanbroker/LoanBrokerFrame.java
+++ src/main/java/forms/loanbroker/LoanBrokerFrame.java
@@ -1,13 +1,17 @@
 package forms.loanbroker;
 
 import java.awt.*;
+import java.util.Properties;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
+import javax.jms.*;
 import shared.bank.*;
 import shared.loan.LoanReply;
 import shared.loan.LoanRequest;
-
 
 public class LoanBrokerFrame extends JFrame {
 
@@ -60,7 +64,9 @@ public class LoanBrokerFrame extends JFrame {
 		contentPane.add(scrollPane, gbc_scrollPane);
 		
 		list = new JList<JListLine>(listModel);
-		scrollPane.setViewportView(list);		
+		scrollPane.setViewportView(list);
+
+		listenToRequests();
 	}
 	
 	 private JListLine getRequestReply(LoanRequest request){    
@@ -98,7 +104,47 @@ public class LoanBrokerFrame extends JFrame {
 
 	private void listenToRequests()
 	{
-		//ToDo
+		Connection connection; // to connect to the JMS
+		Session session; // session for creating consumers
+
+		Destination receiveDestination; //reference to a queue destination
+		MessageConsumer consumer = null; // for receiving messages
+
+		try {
+			Properties props = new Properties();
+			props.setProperty(Context.INITIAL_CONTEXT_FACTORY,
+					"org.apache.activemq.jndi.ActiveMQInitialContextFactory");
+			props.setProperty(Context.PROVIDER_URL, "tcp://localhost:61616");
+
+			// connect to the Destination called “loanRequestQueue”
+			// queue or topic: “queue.loanRequestQueue”
+			props.put(("queue.loanRequestQueue"), " loanRequestQueue");
+
+			Context jndiContext = new InitialContext(props);
+			ConnectionFactory connectionFactory = (ConnectionFactory) jndiContext
+					.lookup("ConnectionFactory");
+			connection = connectionFactory.createConnection();
+			session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+			// connect to the receiver destination
+			receiveDestination = (Destination) jndiContext.lookup("loanRequestQueue");
+			consumer = session.createConsumer(receiveDestination);
+
+			connection.start(); // this is needed to start receiving messages
+
+		} catch (NamingException | JMSException e) {
+			e.printStackTrace();
+		}
+
+
+		try {
+			RequestListener listener = new RequestListener(this);
+			consumer.setMessageListener(listener);
+
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	private void listenToReplys()
@@ -106,12 +152,44 @@ public class LoanBrokerFrame extends JFrame {
 		//ToDo
 	}
 
-	private void sendBankInterestRequest(BankInterestRequest request)
+	public void sendBankInterestRequest(BankInterestRequest request)
 	{
-		//ToDo
+		Connection connection; // to connect to the ActiveMQ
+		Session session; // session for creating messages, producers and
+
+		Destination sendDestination; // reference to a queue destination
+		MessageProducer producer; // for sending messages
+
+		try {
+			Properties props = new Properties();
+			props.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.activemq.jndi.ActiveMQInitialContextFactory");
+			props.setProperty(Context.PROVIDER_URL, "tcp://localhost:61616");
+
+			// connect to the Destination called “bankInterestRequestQueue”
+			// queue or topic: “queue.bankInterestRequestQueue”
+			props.put(("queue.bankInterestRequestQueue"), "bankInterestRequestQueue");
+
+			Context jndiContext = new InitialContext(props);
+			ConnectionFactory connectionFactory = (ConnectionFactory) jndiContext
+					.lookup("ConnectionFactory");
+			connection = connectionFactory.createConnection();
+			session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+			// connect to the sender destination
+			sendDestination = (Destination) jndiContext.lookup("bankInterestRequestQueue");
+			producer = session.createProducer(sendDestination);
+
+			// create a object message containing the request
+			Message msg = session.createObjectMessage(request);
+			// send the message
+			producer.send(msg);
+
+		} catch (NamingException | JMSException e) {
+			e.printStackTrace();
+		}
 	}
 
-	private void sendBankInterestReply(LoanReply reply)
+	public void sendBankInterestReply(LoanReply reply)
 	{
 		//ToDo
 	}
