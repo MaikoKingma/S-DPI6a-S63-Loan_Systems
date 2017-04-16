2e3affd6fe0dba038a0c6f21d5c3b826114017ac
Maiko Kingma <maiko.kingma@outlook.com>
2017-04-16 15:01:12 +0200

    Implemented Scatter-Gather for BankClient

--- src/main/java/forms/bank/LoanBrokerAppGateway.java
+++ src/main/java/forms/bank/LoanBrokerAppGateway.java
@@ -3,32 +3,46 @@ package forms.bank;
 import shared.gateway.*;
 import shared.bank.*;
 
-import javax.jms.JMSException;
-import javax.jms.TextMessage;
+import javax.jms.*;
 import java.util.*;
 
 /**
  * Created by Maiko on 8-4-2017.
  */
 public class LoanBrokerAppGateway extends Gateway {
-    private Map<BankInterestRequest, String> requests;
+    private List<Request> requests;
     private JMSBankFrame frame;
 
     public LoanBrokerAppGateway(JMSBankFrame frame, String bankName) {
         super("bankIntrestReplyQueue", bankName + "RequestQueue");
         this.frame = frame;
-        requests = new HashMap<>();
+        requests = new ArrayList<>();
     }
 
-    public void sendBankIntrestReply(BankInterestRequest request, BankInterestReply reply) {
-         sender.send(sender.createTextMessage(reply, requests.get(request)));
+    public void sendBankIntrestReply(BankInterestRequest bankInterestRequest, BankInterestReply reply) {
+        for (Request request : requests) {
+            if (request.getBankInterestRequest() == bankInterestRequest) {
+                try {
+                    Message message = sender.createTextMessage(reply, request.getCorrolationId());
+                    message.setIntProperty("aggregationId", request.getAggregationId());
+                    sender.send(message);
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+                break;
+            }
+        }
     }
 
     @Override
     protected void processMessage(TextMessage message, String CorrelationId) throws JMSException {
+        Integer aggregationId = message.getIntProperty("aggregationId");
+        if (aggregationId == null)
+            return;
+
         BankInterestRequest bankInterestRequest = new BankInterestRequest();
         bankInterestRequest.fillFromCommaSeperatedValue(message.getText());
-        requests.put(bankInterestRequest, CorrelationId);
+        requests.add(new Request(message.getJMSCorrelationID(), bankInterestRequest, aggregationId));
         frame.add(bankInterestRequest);
     }
 }
